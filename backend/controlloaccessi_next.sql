6a976b11c59de13f39aceade524f0a513c2dfdd1
michelarancan <stage_it@marzottogroup.it>
2026-07-21 14:51:45 +0200

    creazione nuove tabelle nel db

--- backend/controlloaccessi_next.sql
+++ backend/controlloaccessi_next.sql
@@ -165,3 +165,100 @@ CREATE TABLE persone_interne (
 
     FOREIGN KEY (divisione) REFERENCES divisioni(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- INGRESSI_AUTORIZZATI_INTERNI
+
+DROP TABLE IF EXISTS ingressi_autorizzati_interni;
+CREATE TABLE ingressi_autorizzati_interni (
+    id int unsigned PRIMARY KEY AUTO_INCREMENT,
+    persona int unsigned NOT NULL,
+    data_scadenza datetime DEFAULT NULL,
+
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by int unsigned NOT NULL,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_by int unsigned NOT NULL,
+    is_active boolean NOT NULL DEFAULT TRUE,
+
+    FOREIGN KEY (persona) REFERENCES persone_interne(id),
+    UNIQUE (persona)    -- una sola riga per persona
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- AZIENDE
+
+DROP TABLE IF EXISTS aziende;
+CREATE TABLE aziende (
+    id int unsigned PRIMARY KEY AUTO_INCREMENT,
+    ragione_sociale varchar(100) NOT NULL,
+    codice_fiscale varchar(30) NOT NULL,
+    partita_iva varchar(100) NOT NULL,
+
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by int unsigned NOT NULL,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_by int unsigned NOT NULL,
+    is_active boolean NOT NULL DEFAULT TRUE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- TIPI_AZIENDA
+
+DROP TABLE IF EXISTS tipi_azienda;
+CREATE TABLE tipi_azienda (
+    id int unsigned PRIMARY KEY AUTO_INCREMENT,
+    codice varchar(30) NOT NULL,    -- ad esempio: TRASPORTATORE, VISITATORE, FORNITORE...
+    descrizione varchar(200) DEFAULT '',
+
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by int unsigned NOT NULL,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_by int unsigned NOT NULL,
+    is_active boolean NOT NULL DEFAULT TRUE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- AZIENDE_TIPI
+
+DROP TABLE IF EXISTS aziende_tipi;
+CREATE TABLE aziende_tipi (
+    id_azienda int unsigned,
+    id_tipo_azienda int unsigned,
+
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by int unsigned NOT NULL,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_by int unsigned NOT NULL,
+    is_active boolean NOT NULL DEFAULT TRUE,
+
+    PRIMARY KEY (id_azienda, id_tipo_azienda),
+    FOREIGN KEY (id_azienda) REFERENCES aziende(id),
+    FOREIGN KEY (id_tipo_azienda) REFERENCES tipi_azienda(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- INGRESSI_AUTORIZZATI_ESTERNI
+
+DROP TABLE IF EXISTS ingressi_autorizzati_esterni;
+CREATE TABLE ingressi_autorizzati_esterni (
+    id int unsigned PRIMARY KEY AUTO_INCREMENT,
+    sede int unsigned NOT NULL,         -- sede di ingresso
+    cognome varchar(100) NOT NULL,      -- persona esterna
+    nome varchar(100) NOT NULL,
+    azienda int unsigned NOT NULL,    -- azienda di provenienza di questa persona
+    targa varchar(20) DEFAULT NULL,
+    data_scadenza datetime DEFAULT NULL,
+    persona_riferimento int unsigned NOT NULL,  -- persona interna di riferimento
+
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by int unsigned NOT NULL,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_by int unsigned NOT NULL,
+    is_active boolean NOT NULL DEFAULT TRUE,
+
+    UNIQUE (sede, cognome, nome, azienda, data_scadenza),
+    FOREIGN KEY (sede) REFERENCES sedi(id),
+    FOREIGN KEY (persona_riferimento) REFERENCES persone_interne(id),
+    FOREIGN KEY (azienda) REFERENCES aziende(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
