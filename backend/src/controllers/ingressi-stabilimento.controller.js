e4265a8e8f3e276df1292d164f4beb486e8bb1d8
michelarancan <stage_it@marzottogroup.it>
2026-07-22 15:21:26 +0200

    controller ingressi stabilimento

--- backend/src/controllers/ingressi-stabilimento.controller.js
+++ backend/src/controllers/ingressi-stabilimento.controller.js
@@ -16,6 +16,26 @@ function findAll(req, res, next) {
     });
 }
 
+//GET all by data
+function findAllByData(req, res, next) {
+
+    const idSede = req.params.idS;
+    
+    const periodo = {
+        inizioPeriodo: req.query.inizioPeriodo,
+        finePeriodo: req.query.finePeriodo
+    };
+
+
+    service.findAllByData(idSede, periodo, (err, results) => {
+        if(err) {
+            return next(err);
+        }
+
+        res.status(200).json(results);
+    });
+}
+
 //POST 
 function create(req, res, next) {
     //recupera json
@@ -42,25 +62,15 @@ function registerExit(req, res, next) {
 
     const id = req.params.id;
 
-    service.update(id, (err, results) => {
+    service.registerExit(id, (err, results) => {
 
         if (err) {
             return next(err);
         }
 
-        //se query ritorna 'affected 0 rows in total'
-        if (results.affectedRows === 0) {
-            const error = new Error('Ingresso allo stabilimento non trovato');
-
-            error.status = 404;
-            error.code = 'INGRESSO_NOT_FOUND';
-
-            return next(error);
-        }
-
         res.status(200).json({
             success: true,
-            message: 'Ingresso allo stabilimento aggiornato con successo'
+            message: 'Uscita registrata con successo'
         });
 
     });
@@ -84,4 +94,27 @@ function search(req, res, next) {
     );
 }
 
-module.exports = { findAll, create, registerExit, search };
+//SEARCH by data
+function searchByData(req, res, next) {
+
+    const idSede = req.params.idS;
+    
+    const periodo = {
+        inizioPeriodo: req.query.inizioPeriodo,
+        finePeriodo: req.query.finePeriodo
+    };
+
+    const campo = req.query.campo;
+    const valore = req.query.valore;
+
+    service.searchByData(idSede, periodo, campo, valore, (err, results) => {
+        if(err) {
+            return next(err);
+        }
+
+        res.status(200).json(results);
+    }
+    );
+}
+
+module.exports = { findAll, findAllByData, create, registerExit, search, searchByData };
